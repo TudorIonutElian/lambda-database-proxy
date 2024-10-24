d97f199e3f3cb83f7fd597073797bc0fac159584
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-24 19:23:08 +0300

    LDP -- add VPC subnet

---
+++ infrastructure/vpc_subnets.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "lambda_database_proxy_subnet" {
+  vpc_id                  = aws_vpc.lambda_database_proxy_vpc
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "lambda_database_proxy_subnet"
+    Project = "lambda-database-proxy"
+  }
+}
