f5d8e959e1cfb9914d5b70e373b4bac395cd5667
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-24 19:25:30 +0300

    LDP -- fix VPC id typo

--- infrastructure/vpc_subnets.tf
+++ infrastructure/vpc_subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "lambda_database_proxy_subnet" {
-  vpc_id                  = aws_vpc.lambda_database_proxy_vpc
+  vpc_id                  = aws_vpc.lambda_database_proxy_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = false
