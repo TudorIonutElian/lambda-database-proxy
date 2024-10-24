c05ee1f6eebc909a55706d48930e1d1d236c96a6
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-24 19:06:18 +0300

    LDP -- setup initial terraform files

---
+++ infrastructure/data-archieve.tf
@@ -0,0 +1,14 @@
+/*
+  * data "archive_file" is used to create a zip file from the source file.
+  * The source file is the index.js file in the lambda directory.
+  * The output path is the lambda_function_payload.zip file.
+   - type: The type of archive to create (zip in this case).
+    - source_file: The path to the file or directory to be archived.
+    - output_path: The path to save the archive file.
+*/
+
+data "archive_file" "lambda_dedicated_infrastructure_archive" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda-database-proxy/index.js"
+  output_path = "${path.module}/lambda_database_proxy.zip"
+}
