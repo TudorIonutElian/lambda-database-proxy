dad33a1e4fd30fa3e07854f7fa2eb1fb68c8f5d2
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-24 19:52:31 +0300

    LDP -- change ip range

--- infrastructure/vpc_subnets.tf
+++ infrastructure/vpc_subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "lambda_database_proxy_subnet_1a" {
   vpc_id                  = aws_vpc.lambda_database_proxy_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = false
 
