77b31dde2ff6a40e50c72920af46f72d16b43f85
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-24 19:45:26 +0300

    LDP -- add 2 availability zones

--- infrastructure/vpc_subnets.tf
+++ infrastructure/vpc_subnets.tf
@@ -1,11 +1,25 @@
-resource "aws_subnet" "lambda_database_proxy_subnet" {
+resource "aws_subnet" "lambda_database_proxy_subnet_1a" {
   vpc_id                  = aws_vpc.lambda_database_proxy_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = false
 
   tags = {
-    Name    = "lambda_database_proxy_subnet"
+    Name    = "lambda_database_proxy_subnet-1a"
+    Project = "lambda-database-proxy"
+  }
+}
+
+// add more subnets here
+
+resource "aws_subnet" "lambda_database_proxy_subnet_1b" {
+  vpc_id                  = aws_vpc.lambda_database_proxy_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "lambda_database_proxy_subnet-1b"
     Project = "lambda-database-proxy"
   }
 }
