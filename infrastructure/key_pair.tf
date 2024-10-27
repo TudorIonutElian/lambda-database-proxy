a42dd2249625e6b7de2eee2a0d2430c2e6721db5
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-27 20:09:22 +0200

    Add the created key

--- infrastructure/key_pair.tf
+++ infrastructure/key_pair.tf
@@ -5,12 +5,5 @@ resource "tls_private_key" "privatekey" {
 
 resource "aws_key_pair" "database_jumpbox_key" {
   key_name   = "database_jumpbox_key"      
-  public_key = tls_private_key.privatekey.public_key_openssh
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo '${tls_private_key.privatekey.private_key_pem}' > ${path.module}/database_jumpbox.pem
-      echo "Private key saved to ${path.module}/database_jumpbox.pem"
-    EOT
-  }
+  public_key = file("${path.module}/database_jumpbox_key.pub")
 }
