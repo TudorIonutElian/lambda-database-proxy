9fd944a3bd5bb8553d4cf953e9429302db6e1997
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-27 13:24:48 +0200

    Add the tags correclty

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -2,12 +2,13 @@
 /**********************************************************
   # Create Ec2 Instance called iris_tf_demo_ec2_instance
 **********************************************************/
-resource "aws_instance" "cloudwatch_ec2_instances" {
+resource "aws_instance" "rds_jumpbox" {
   ami           = data.aws_ami.ec2_ami_filter.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.database_jumpbox_key.key_name
 
   tags = {
-    Name = "ec2-cloudwatch-instance"
+    Name = "lambda-database-proxy-func"
+    Project = "lambda-database-proxy"
   }
 }
