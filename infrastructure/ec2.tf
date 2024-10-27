70189765e766974bbe63256de34d3ca4bb850048
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-27 20:12:36 +0200

    Add the ec2 inside subnet

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "rds_jumpbox" {
   ami           = data.aws_ami.ec2_ami_filter.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.database_jumpbox_key.key_name
+  subnet_id = aws_subnet.lambda_database_proxy_subnet_1a.id
+  vpc_security_group_ids = [aws_security_group.lambda_database_proxy_mysql_sg.id]
 
   tags = {
     Name = "lambda-database-proxy-func"
