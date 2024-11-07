b7f9d39f32fddb1eb3ac84d3776007e759db8c4e
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-11-07 21:31:42 +0200

    remove unneded sg

--- infrastructure/security_groups.tf
+++ infrastructure/security_groups.tf
@@ -5,13 +5,6 @@ resource "aws_security_group" "mysql_cluster_sg" {
   vpc_id      = aws_vpc.lambda_database_proxy_vpc.id
 }
 
-// add a security group for the ec2 jumpbox
-resource "aws_security_group" "ec2_jumpbox_sg" {
-  name        = "ec2-jumpbox-sg"
-  description = "Security group for EC2 jumpbox"
-  vpc_id      = aws_vpc.lambda_database_proxy_vpc.id
-}
-
 // add a security group for the rds proxy
 resource "aws_security_group" "rds_proxy_sg" {
   name        = "rds-proxy-sg"
@@ -24,18 +17,6 @@ resource "aws_security_group" "lambda_sg" {
   name        = "lambda-sg"
   description = "Security group for Lambda function"
   vpc_id      = aws_vpc.lambda_database_proxy_vpc.id
-}
-
-// Define security group rules
-
-// Allow inbound traffic from EC2 jumpbox to MySQL cluster on port 3306
-resource "aws_security_group_rule" "mysql_cluster_ingress_from_ec2_jumpbox" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  security_group_id = aws_security_group.mysql_cluster_sg.id
-  source_security_group_id = aws_security_group.ec2_jumpbox_sg.id
 }
 
 // Allow inbound traffic from RDS proxy to MySQL cluster on port 3306
